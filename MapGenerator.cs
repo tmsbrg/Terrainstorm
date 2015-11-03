f2cd126396fae835cc5d0d09a81707de354ffbf3
Thomas van der Berg <ik@thomasvanderberg.nl>
2015-11-03 11:23:30 +0100

    initial commit

---
+++ MapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using libmapgen;
+
+public class MapGenerator : MonoBehaviour {
+
+    Ruleset ruleset;
+
+	// Use this for initialization
+	void Start () {
+	    Debug.Log("meow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
