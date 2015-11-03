235b8731d041ee12aa8f6090ffca6a7ee20b942e
Thomas van der Berg <ik@thomasvanderberg.nl>
2015-11-03 14:36:03 +0100

    made it possible to receive a generated map in a function

---
+++ IMapHandler.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using libmapgen;
+
+public interface IMapHandler {
+    // TODO: reexport MapArea so people don't need to use 'using libmapgen'
+    void OnMapFinish(MapArea map);
+}
--- MapGenerator.cs
+++ MapGenerator.cs
@@ -1,15 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 using libmapgen;
 
 public class MapGenerator : MonoBehaviour {
+
+    public GameObject mapHandler = null;
+    MapArea mapArea = null;
 
     Ruleset ruleset;
 
 	// Use this for initialization
 	void Start () {
-	    Debug.Log("meow");
+
+        mapArea = GenerateMap();
+
+        if (mapHandler != null) {
+            IMapHandler handler = mapHandler.GetComponent<IMapHandler>();
+
+            if (handler != null) {
+                handler.OnMapFinish(mapArea);
+            } else {
+                Debug.LogWarning("map handler lacks IMapHandler");
+            }
+        }
 	}
+
+    MapArea GenerateMap() {
+        // TODO
+	    Debug.Log("Build map!");
+        return new MapArea(10, 10);
+    }
 	
 	// Update is called once per frame
 	void Update () {
